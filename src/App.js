b34a3116ab79a09d8062ce70122c3423b79383b9
Ayush Gupta <ayushgupta.hestbit@gmail.com>
2022-11-15 12:43:19 +0530

    razorpay

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from "react";
+import useRazorpay from "react-razorpay";
 
 function App() {
+  const user = {
+    mobile: 9045973853,
+    email: 'ayush.gupta@hestabit.in',
+    name: "Ayush Gupta",
+    address: "sector 7, noida"
+  }
+
+  const shop = {
+    name: "ABCD Shop",
+    product_description: "Test Description"
+  }
+  const amount = 100;
+
+  const options = {
+    key: "rzp_test_HJG5Rtuy8Xh2NB",
+    amount: `${amount}`,
+    name: shop?.name,
+    description: shop?.product_description,
+    image: "https://cdn.razorpay.com/logos/7K3b6d18wHwKzL_medium.png",
+    handler: function (response) {
+      console.log(response)
+      alert(response.razorpay_payment_id);
+    },
+    prefill: {
+      name: user.name,
+      contact: user.mobile,
+      email: user.email,
+    },
+    notes: {
+      address: user.address
+    },
+    theme: {
+      color: "#F37254",
+      hide_topbar: false
+    }
+  };
+
+  const openPayModal = options => {
+    let rzp1 = new window.Razorpay(options);
+    rzp1.open();
+  };
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://checkout.razorpay.com/v1/checkout.js";
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => openPayModal(options)}>Pay {amount / 100}₹</button>
       </header>
     </div>
   );
